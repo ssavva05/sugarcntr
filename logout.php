8d2ff4e186fd6c7c8ae52e0729296826338ed199
ssavva05 <ssavva05@ucy.ac.cy>
2018-10-10 01:58:41 +0300

    up

--- logout.php
+++ logout.php
@@ -4,12 +4,10 @@
 
 
 if (isset($_SESSION)) {
-    setcookie("type", "", time() - 3600);
     session_destroy();
-} else {
-    setcookie("type", "", time() - 3600);
-    header("location:login.php");
 }
+setcookie("type", "", time() - 3600);
+header("location:login.php");
 
 //setcookie("type", "", time()-3600);
 //session_destroy();
