da19ebc545d3068a638235828ce5aad836a7b06b
ssavva05 <ssavva05@ucy.ac.cy>
2018-10-10 01:55:00 +0300

    up

--- logout.php
+++ logout.php
@@ -1,8 +1,8 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+//logout.php
+setcookie("type", "", time()-3600);
+session_destroy();
+header("location:login.php");
 
+?>
